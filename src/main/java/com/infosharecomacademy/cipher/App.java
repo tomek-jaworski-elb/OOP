128996276c4c8f520aafdb5aa0d0584c0b6eb9ac
Tomek Jaworski <tj.toja@gmai.com>
2022-03-01 09:55:56 +0100

    Implemented encrypt

--- src/main/java/com/infosharecomacademy/cipher/App.java
+++ src/main/java/com/infosharecomacademy/cipher/App.java
@@ -1,4 +1,9 @@
 package com.infosharecomacademy.cipher;
 
 public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Ceaser Cipher");
+        System.out.println(new CeaserCipher().encrypt("abcdef abc xyz"));
+    }
 }
--- src/main/java/com/infosharecomacademy/cipher/CeaserCipher.java
+++ src/main/java/com/infosharecomacademy/cipher/CeaserCipher.java
@@ -1,13 +1,46 @@
 package com.infosharecomacademy.cipher;
 
-public class CeaserCipher extends Cipher{
+import java.util.Scanner;
+
+public class CeaserCipher extends Cipher {
+
+
+    private final static String ABC = "abcdefghijklmnopqrstuwxyz";
+
+
     @Override
     String encrypt(String text) {
-        return null;
+        System.out.println(text);
+        int offset = 3;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c != ' ') {
+                int position = ABC.indexOf(c);
+                position += offset;
+                if (position >= ABC.length()) {
+                    position = position - ABC.length();
+                }
+                result.append(String.valueOf(ABC.charAt(position)));
+            } else {
+                result.append(" ");
+            }
+        }
+        return result.toString();
     }
 
     @Override
     String decrypt(String text) {
         return null;
+    }
+
+    private String getInput(String textToPrint) {
+        System.out.println(textToPrint);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public void run() {
+        String inputText = getInput("enter text");
     }
 }
