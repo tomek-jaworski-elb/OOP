607ab08d258a65c0e1d9427faf798cc6c8f4bc87
Tomek Jaworski <tj.toja@gmail.com>
2022-03-06 09:26:07 +0100

    Lotto e-2

--- src/main/java/com/infosharecomacademy/lotto/App.java
+++ src/main/java/com/infosharecomacademy/lotto/App.java
@@ -8,9 +8,9 @@ import java.util.Set;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Podaj param1: ");
+        System.out.print("Podaj ilosc liczb: ");
         int param1 = scanner.nextInt();
-        System.out.print("Podaj param2: ");
+        System.out.print("Podaj zakres liczb: ");
 
         int param2 = scanner.nextInt();
         System.out.println(param1 + " | "+param2);
