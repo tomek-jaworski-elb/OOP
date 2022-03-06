04098ea27761fff5b497558d9a080ac715db6065
Tomek Jaworski <tj.toja@gmail.com>
2022-03-06 09:24:58 +0100

    Lotto e-1

--- src/main/java/com/infosharecomacademy/lotto/App.java
+++ src/main/java/com/infosharecomacademy/lotto/App.java
@@ -1,6 +1,9 @@
 package com.infosharecomacademy.lotto;
 
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
@@ -9,8 +12,20 @@ public class App {
         int param1 = scanner.nextInt();
         System.out.print("Podaj param2: ");
 
-
         int param2 = scanner.nextInt();
-        System.out.println(param1 + "|"+param2);
+        System.out.println(param1 + " | "+param2);
+        System.out.println(lotto(param1,param2));
     }
+
+    private static Set<Integer> lotto(int numberCount, int range) {
+        Set<Integer> set = new HashSet<>();
+        do {
+            Random random = new Random();
+            int number = random.nextInt(range)+1;
+            set.add(number);
+        } while (set.size()<numberCount);
+        return set;
+    }
+
+
 }
