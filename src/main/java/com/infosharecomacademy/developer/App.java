ef97da81348844f4c12bb90c85f50faa99ca7fc8
Tomek Jaworski <tj.toja@gmai.com>
2022-03-16 18:41:49 +0100

    task 2 code cleanuped

--- src/main/java/com/infosharecomacademy/developer/App.java
+++ src/main/java/com/infosharecomacademy/developer/App.java
@@ -1,9 +1,6 @@
 package com.infosharecomacademy.developer;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class App {
 
@@ -22,13 +19,13 @@ public class App {
         developers.add(new Developer(1L, "Tomek", Skill.PYTHON));
         developers.add(new Developer(55L, "Tomek", Skill.JAVA));
 
-        Map<Skill, List<Long>> map = new HashMap<>();
+        Map<Skill, List<Long>> map = new EnumMap<>(Skill.class);
 
         for (Developer developer : developers) {
             Skill key = developer.getSkill();
             map.putIfAbsent(developer.getSkill(), new ArrayList<>());
             List<Long> tmp = map.get(key);
-            tmp.add(developer.getID());
+            tmp.add(developer.getId());
             map.put(developer.getSkill(), tmp);
         }
 
