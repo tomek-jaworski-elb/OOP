3b9351ec7db777fbd0c3a97129c4bbe527d29b3a
Tomek Jaworski <tj.toja@gmai.com>
2022-03-24 19:07:18 +0100

    Implemented concat streams

--- src/main/java/com/infosharecomacademy/jsonfromurl/App.java
+++ src/main/java/com/infosharecomacademy/jsonfromurl/App.java
@@ -1,20 +1,33 @@
 package com.infosharecomacademy.jsonfromurl;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class App {
 
-    private static final URI API_ADDRES = URI.create("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a");
+    private static final URI API_ADDRESS_A = URI.create("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a");
+    private static final URI API_ADDRESS_M = URI.create("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=m");
+    private static final URI API_ADDRESS_T = URI.create("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=t");
 
     public static void main(String[] args) {
         Data data = new Data();
-        String dataApi = data.getData(API_ADDRES);
-        System.out.println(dataApi);
-        ReadJson readJson = new ReadJson(dataApi);
-        Drinks jsonData = readJson.getData();
-        jsonData.getDrinks()
-                .stream()
-                .sorted((o1, o2) -> Integer.parseInt(o2.getIdDrink()) - Integer.parseInt(o1.getIdDrink()))
+        String data0 = data.getData(API_ADDRESS_M);
+        ReadJson readJson0 = new ReadJson(data0);
+
+        String data1 = data.getData(API_ADDRESS_A);
+        ReadJson readJson1 = new ReadJson(data1);
+
+        String data2 = data.getData(API_ADDRESS_T);
+        ReadJson readJson2 = new ReadJson(data2);
+
+        System.out.println("*******************");
+        List<Drink> drinkList = Stream.concat(readJson0.getData().getDrinks().stream(),
+                readJson1.getData().getDrinks().stream()).toList();
+        drinkList = Stream.concat(drinkList.stream(), readJson2.getData().getDrinks().stream()).toList();
+        drinkList.stream()
+                .sorted((o1, o2) -> Integer.parseInt(o1.getIdDrink()) - Integer.parseInt(o2.getIdDrink()))
                 .forEach(System.out::println);
+        System.out.println("Size: " + (long) drinkList.size());
     }
 }
--- src/main/java/com/infosharecomacademy/jsonfromurl/Drink.java
+++ src/main/java/com/infosharecomacademy/jsonfromurl/Drink.java
@@ -1,9 +1,16 @@
 package com.infosharecomacademy.jsonfromurl;
 
+import java.util.Objects;
+
 public class Drink {
 
     private String idDrink;
     private String strDrink;
+    private String strCategory;
+    private String strAlcoholic;
+    private String strGlass;
+    private String dateModified;
+
 
     public String getIdDrink() {
         return idDrink;
@@ -14,10 +21,27 @@ public class Drink {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Drink drink = (Drink) o;
+        return Objects.equals(idDrink, drink.idDrink) && Objects.equals(strDrink, drink.strDrink) && Objects.equals(strCategory, drink.strCategory) && Objects.equals(strAlcoholic, drink.strAlcoholic) && Objects.equals(strGlass, drink.strGlass) && Objects.equals(dateModified, drink.dateModified);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idDrink, strDrink, strCategory, strAlcoholic, strGlass, dateModified);
+    }
+
+    @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("Drink{");
         sb.append("idDrink='").append(idDrink).append('\'');
         sb.append(", strDrink='").append(strDrink).append('\'');
+        sb.append(", strCategory='").append(strCategory).append('\'');
+        sb.append(", strAlcoholic='").append(strAlcoholic).append('\'');
+        sb.append(", strGlass='").append(strGlass).append('\'');
+        sb.append(", dateModified='").append(dateModified).append('\'');
         sb.append('}');
         return sb.toString();
     }
